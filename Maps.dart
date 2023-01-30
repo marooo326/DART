3ef3fd4bcd716ef014a548da006bfc8581861ef7
marooo326 <ktnqkr01@gmail.com>
2023-01-30 23:51:19 +0900

    Add Maps.dart

---
+++ Maps.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Map의 기본 형태
+  // Map<key,value> 변수이름 과 같은 형태로 선언해서 사용한다.
+  Map<int, String> data = {
+    1: "hello",
+    2: "hi",
+    3: "bye",
+  };
+
+  // Map의 key와 value는 어떤 object도 가능하다.
+  // 다만 API로 얻은 형태와 같은 데이터는 class를 사용하는 것이 바람직하다.
+}
